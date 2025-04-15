287bdb7c8a47be34271259aaa765a30f8f032a63
Sisqui <aries94leifu@gmail.com>
2025-04-15 09:39:58 +0200

    update launch file to get 2D pixel coordinates

--- person_detect/launch/person_detect.launch.py
+++ person_detect/launch/person_detect.launch.py
@@ -95,6 +95,38 @@ def generate_launch_description():
         ]
     )
 
+    ray_water = Node(
+        package='liquid_detection',  
+        executable='water',  
+        name='ray_water',
+        output='screen',
+        remappings=[
+            ('/tf', '/summit/tf'),
+            ('/tf_static', '/summit/tf_static'),
+            ('/leakage_marker', '/summit/leakage_marker'),
+        ],
+        parameters=[
+            {'world_frame': 'map'},
+            {'use_sim_time': True},
+            {'pixel_topic': '/leakage_pixel_coords'},
+            {'camera_info_topic': '/summit/color/camera_info'},
+            {'output_topic': '/leakage_ground_point'},
+            {'tf_lookup_timeout_sec': 1.0},]
+    )
+
+    pixel_pub = Node(
+        package='liquid_detection',  
+        executable='pixel_water',  
+        name='pixel_water',
+        output='screen',
+        remappings=[
+            ('/leakage_annotated_image', '/summit/leakage_annotated_image'),
+        ],
+        parameters=[
+            {'image_topic': '/summit/color/image'},
+            {'camera_info_topic': '/summit/color/camera_info'},]
+    )
+
     tracker_node = Node(
         package='depthai_examples',
         executable='tracker_yolov4_spatial_node',
@@ -144,4 +176,6 @@ def generate_launch_description():
         tracker_node,
         person_detect_node,
         #rviz_node
+        ray_water,
+        pixel_pub
     ])
