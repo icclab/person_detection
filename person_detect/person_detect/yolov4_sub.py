4fe97fb0da232b6b584f69f0ac2879155fdaa221
Gaurav Kapoor <gkapoordelhi@gmail.com>
2025-05-09 15:36:06 +0200

    update round trip time

--- person_detect/person_detect/yolov4_sub.py
+++ person_detect/person_detect/yolov4_sub.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge
 import time
 from ultralytics import YOLO
@@ -9,11 +9,20 @@ import torch
 import csv
 from datetime import datetime
 from detections_msg.msg import Detections
+from rclpy.qos import QoSProfile, ReliabilityPolicy
+from rclpy.executors import MultiThreadedExecutor
 
 class YoloV8nNode(Node):
     def __init__(self):
         super().__init__('yolo_node_sub')
+        
+        qos = QoSProfile(
+            depth=10,
+            reliability=ReliabilityPolicy.RELIABLE
+        )
+
         self.subscription = self.create_subscription(Detections, '/oak/yolo/detections', self.listener_callback, 10)
+        self.subscription2 = self.create_subscription(CompressedImage, '/oak/rgb/image_raw/dynamic/compressed', self.listener_callback2, qos)
         
         self.start_time_str = time.strftime("%d-%m-%Y_%H-%M-%S")
         self.output_file = f"yolo_v4_{self.start_time_str}.csv"
@@ -24,19 +33,24 @@ class YoloV8nNode(Node):
         self.csvfile.flush()
 
         self.get_logger().info(f"Logging to: {self.output_file}")
+
+        self.t1 = 0
+
+    def listener_callback2(self, msg):
+        self.t1 = time.time()
         
     def listener_callback(self, msg):
         self.get_logger().info("Received yolo detection message")
 
         self.get_logger().info(f"[YOLO] CLASS ID: {msg.class_id}")
-        self.get_logger().info(f"[YOLO] INFERENCE TIME: {msg.inference_time_s:.2f} sec")
         self.get_logger().info(f"[YOLO] ACCURACY: {msg.accuracy_percent:.2f} %")
         self.get_logger().info(f"[YOLO] PAYLOAD SIZE: {msg.payload_bytes} bytes")
         self.get_logger().info(f"[YOLO] CUDA: {msg.cuda}")
         
         # self.writer.writerow([time.time(), msg.class_id, msg.inference_time_s, msg.accuracy_percent, msg.payload_bytes, msg.cuda])
 
-        new_inference_time = time.time() - (msg.header.stamp.sec + msg.header.stamp.nanosec * 1e-9) 
+        new_inference_time = time.time() - self.t1
+        self.get_logger().info(f"[YOLO] INFERENCE TIME: {new_inference_time:.2f} sec")
 
         self.writer.writerow([time.time(), msg.class_id, new_inference_time, msg.accuracy_percent, msg.payload_bytes, msg.cuda])
         
@@ -46,13 +60,18 @@ class YoloV8nNode(Node):
         if self.csvfile:
             self.csvfile.close()
 
-
 def main(args=None):
     rclpy.init(args=args)
     node = YoloV8nNode()
-    rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()
+
+    executor = MultiThreadedExecutor()
+    executor.add_node(node)
+
+    try:
+        executor.spin()
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
