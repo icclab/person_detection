048b8a7a5546dd4af25403673f3e081332670bd3
Gaurav Kapoor <gkapoordelhi@gmail.com>
2025-06-05 12:02:23 +0200

    updated comp file

--- person_detect/launch/img_pub_freq_1_comp_80.launch.py
+++ person_detect/launch/img_pub_freq_1_comp_80.launch.py
@@ -1,13 +1,21 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument,IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
  
 def generate_launch_description():
+
+    person_detect_pkg = get_package_share_directory("person_detect")
+
+    compress_level = LaunchConfiguration("compress")
+
     return LaunchDescription([
         DeclareLaunchArgument(
             'image_folder',
-            default_value='/home/icc-nano/energy_ws/src/test_person/',
+            default_value='/home/icc-nano/energy_ws/src/MOT20-01/img1/',
             description='Folder containing image files to publish'
         ),
         DeclareLaunchArgument(
@@ -15,25 +23,35 @@ def generate_launch_description():
             default_value='1.0',
             description='Frequency (Hz) to publish images'
         ),
+
+        DeclareLaunchArgument(
+            'fps',
+            default_value='30.0',
+            description='Frames per second for image publishing'
+        ),
+
         DeclareLaunchArgument(
             'compress',
             default_value='80',
             description='compress'
         ),
-        DeclareLaunchArgument(
-            'duration',
-            default_value='200.0',
-            description='Total duration (seconds) to run the publisher'
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(person_detect_pkg, "launch", "compress.launch.py")
+            ),
+            launch_arguments={"compress": compress_level}.items(),
         ),
+
         Node(
             package='pub_yolo',
-            executable='image_pub_comp',
+            executable='image_pub',
             name='image_publisher',
             parameters=[
                 {'image_folder': LaunchConfiguration('image_folder')},
                 {'frequency': LaunchConfiguration('frequency')},
-                {'duration': LaunchConfiguration('duration')},
-                {'compress': LaunchConfiguration('compress')},
+                {'compress': compress_level},
+                {'fps': LaunchConfiguration('fps')}
             ],
             output='screen'
         )
