8743e58e106815ffc4dbc1a8edad1f808fc686cd
Gaurav Kapoor <gkapoordelhi@gmail.com>
2025-06-06 12:10:23 +0200

    added dynamic freq pub

--- pub_yolo/pub_yolo/dynamic_freq.py
+++ pub_yolo/pub_yolo/dynamic_freq.py
@@ -9,27 +9,34 @@ import csv
 import math
 import subprocess
 import signal
+from rclpy.executors import MultiThreadedExecutor
 
 class ImagePublisher(Node):
     def __init__(self):
         super().__init__('image_publisher')
 
-        self.declare_parameter("image_folder", "/home/icc-nano/energy_ws/src/test_person_1/")
+        self.declare_parameter("image_folder", "/home/icc-nano/energy_ws/src/MOT20-01/img1/")
         self.declare_parameter("csv_file", "/home/icc-nano/energy_ws/src/workload_1.csv")
-        self.declare_parameter("compress", 80) 
+
+        self.declare_parameter("compress", 100) 
+        self.declare_parameter("fps", 30.0)  # default: 30 Hz
 
         self.image_folder = self.get_parameter("image_folder").get_parameter_value().string_value
-        self.csv_file = self.get_parameter("csv_file").get_parameter_value().string_value
+
+        self.fps = self.get_parameter("fps").get_parameter_value().double_value
+
         self.compress = self.get_parameter("compress").get_parameter_value().integer_value
 
         self.publisher = self.create_publisher(Image, '/oak/rgb/image_raw', 10)
+
+        self.csv_file = self.get_parameter("csv_file").get_parameter_value().string_value
  
         self.bridge = CvBridge()
 
+        # === Count images ===
         self.image_files = sorted([
-            os.path.join(self.image_folder, f)
-            for f in os.listdir(self.image_folder)
-            if f.endswith(('.jpg', '.png'))
+            f for f in os.listdir(self.image_folder)
+            if f.endswith(".jpg") or f.endswith(".png")
         ])
 
         if len(self.image_files) < 1:
@@ -38,7 +45,6 @@ class ImagePublisher(Node):
 
         # Read frequency list
         self.frequencies = []
-        # self.compressions = []
         with open(self.csv_file, 'r') as f:
             reader = csv.DictReader(f)
             for row in reader:
@@ -49,6 +55,7 @@ class ImagePublisher(Node):
 
         self.current_freq_index = 0
         self.image_index = 0
+        self.add = 0 
         self.images_published_in_current_block = 0
 
         self.start_frequency_block()
@@ -74,40 +81,48 @@ class ImagePublisher(Node):
     def timer_callback(self):
         if self.images_published_in_current_block >= self.images_to_publish:
             self.timer.cancel()
-            # self.stop_compression_node()
             self.current_freq_index += 1
+            self.image_index -= round(self.fps / self.current_freq)
+            self.image_index += 1
             self.start_frequency_block()
             return
 
-        # Loop image index if we reach the end
-        img_path = self.image_files[self.image_index % self.total_images]
-        self.image_index += 1
+        filename = self.image_files[self.image_index]
+        img_path = os.path.join(self.image_folder, filename)
 
         cv_image = cv2.imread(img_path)
         if cv_image is None:
             self.get_logger().warning(f'Failed to load image: {img_path}')
             return
 
-        self.get_logger().info(
-            f'[{self.current_freq} Hz] Published image {self.image_index + 1}: {os.path.basename(img_path)[0]}'
-        )
-
         log_name = os.path.basename(img_path)
+
+        self.get_logger().info(
+            f'[{self.current_freq} Hz] Published image {self.image_index}: {log_name}'
+        )
 
         ros_image = self.bridge.cv2_to_imgmsg(cv_image, encoding='bgr8')
         ros_image.header.stamp = self.get_clock().now().to_msg()
-        ros_image.header.frame_id = log_name[0] + "," + log_name + "," + str(self.compress) + "," + str(self.current_freq)
+        ros_image.header.frame_id = log_name + "," + str(self.compress) + "," + str(self.current_freq)
         self.publisher.publish(ros_image)
         
+        self.image_index += round(self.fps / self.current_freq)
         self.images_published_in_current_block += 1
-
 
 def main(args=None):
     rclpy.init(args=args)
     node = ImagePublisher()
-    rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()
+    
+    executor = MultiThreadedExecutor()
+    executor.add_node(node)
+
+    try:
+        executor.spin()
+    except Exception as e:
+        print(f"[ImagePublisher Error] {e}")
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
